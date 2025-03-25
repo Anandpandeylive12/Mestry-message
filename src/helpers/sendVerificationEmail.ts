6c05fcc9d95d914cc5e48be163b0c95d6f4685e1
Anand Pandey <anandpandeylive12@gmail.com>
2025-03-25 23:49:58 +0530

    first commit

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,30 @@
+import { resend } from "@/lib/resend";
+
+import { ApiResponse } from "@/types/ApiResponse";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    const response = await resend.emails.send({
+      from: "no-reply@mysteryauth.com", // ✅ Use a verified email from Resend
+      to: email,
+      subject: "Mystery Message Verification Code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
+    console.log("Resend response:", response);
+
+    return { success: true, message: "Verification email sent successfully." };
+  } catch (emailError: any) {
+    console.error(
+      "Error sending verification email:",
+      emailError.response?.data || emailError.message || emailError
+    );
+    return { success: false, message: "Failed to send verification email." };
+  }
+}
